4cdfed01e9a6e13fbedde30a9602c697d5728701
matheus freitas <mgsoutodelira@gmail.com>
2025-05-21 10:54:56 -0300

    config .env

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,16 @@
 import { knex as setupKnex } from 'knex';
+import { env } from "./env"
 
-export const knex = setupKnex({
+export const config = {
   client: 'sqlite3',
   connection: {
-    filename: './tmp/app.db',
-  }
-})
+    filename: env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: './db/migrations'
+  },
+}
+
+export const knex = setupKnex(config);
