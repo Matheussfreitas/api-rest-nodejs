a9098c40b8a9b8b642d298686c17b7869a272f96
matheus freitas <mgsoutodelira@gmail.com>
2025-05-21 13:55:22 -0300

    add routes

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
-import { knex as setupKnex } from 'knex';
+import { Knex, knex as setupKnex } from 'knex';
 import { env } from "./env"
 
-export const config = {
+export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
     filename: env.DATABASE_URL,
